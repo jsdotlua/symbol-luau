89851b55a421f2c38a8ab464a062d53db8735385
jeparlefrancais <jeparlefrancais21@gmail.com>
2023-11-16 23:09:34 -0500

    automatically put package version in wally.toml

--- scripts/build-wally-package.sh
+++ scripts/build-wally-package.sh
@@ -14,8 +14,14 @@ cp -r src/ build/wally
 
 darklua process build/wally/src build/wally/src
 
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
 cp default.project.json build/wally
-cp wally.toml build/wally
+cat wally.toml | sed 's/#VERSION#/'"$PACKAGE_VERSION"'/' > build/wally/wally.toml
 cp LICENSE.txt build/wally
 
 wally package --project-path build/wally --list
